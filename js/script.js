9863b39ac384d0b1385747b8b5d2e77998544c37
Alejandro Diaz <alejandrodiazpugh@gmail.com>
2022-04-06 17:30:29 -0500

    Simplificación de proceso de captura de datos, incorporación en un solo array.

--- js/script.js
+++ js/script.js
@@ -1,43 +1,36 @@
 // CAPTURA DE DATOS PARA PERSONAS INTERESADAS EN PROGRAMAS EN LINEA
 
-const USUARIOS_CORRER = [];
-const USUARIOS_PESAS = [];
-const USUARIOS_YOGA =[];
+const USUARIOS_PROGRAMAS = [];
+const CURSOS = [];
 
-function nuevoUsuarioCorrer() {
-    let nombre = prompt("Por favor ingresa tu nombre para inscribirte al curso 'Couch To 5K'")
-    if(nombre !== "") {
-        let correrAlert = alert(`Gracias ${nombre}, nos contactaremos pronto para dictarte el proceso para tu inscripción`);
-        USUARIOS_CORRER.push(nombre);
-    } else {
-        let errorAlert = alert("Por favor vuelve a intentarlo ingresando un nombre en la casilla.");
-    }
-}
-function nuevoUsuarioYoga() {
-    let nombre = prompt("Por favor ingresa tu nombre para inscribirte al curso 'Hatha Yoga'")
-    if(nombre !== "") {
-        let correrAlert = alert(`Gracias ${nombre}, nos contactaremos pronto para dictarte el proceso para tu inscripción`);
-        USUARIOS_YOGA.push(nombre);
-    } else {
-        let errorAlert = alert("Por favor vuelve a intentarlo ingresando un nombre en la casilla.");
-    }
-}
+//Cargamos los cursos actuales y dejamos espacio para más oferta de cursos a futuro.
+nuevoCurso = (entrada) => CURSOS.push(entrada);
+nuevoCurso("Couch to 5K");
+nuevoCurso("Hatha Yoga");
+nuevoCurso("Boring But Big");
 
-function nuevoUsuarioPesas() {
-    let nombre = prompt("Por favor ingresa tu nombre para inscribirte al curso 'Boring But Big'")
-    if(nombre !== "") {
-        let correrAlert = alert(`Gracias ${nombre}, nos contactaremos pronto para dictarte el proceso para tu inscripción`);
-        USUARIOS_PESAS.push(nombre);
-    } else {
-        let errorAlert = alert("Por favor vuelve a intentarlo ingresando un nombre en la casilla.");
-    }
-}
+//Creamos la lista de clientes por objetos.
+class Cliente {
+    constructor(nombre, curso) {
+        this.nombre = nombre;
+        this.curso = curso;
+    }    
+}  
 
+//Pasamos la clase por una función que agrega objetos al array de usuarios con el botón
+
+function agregarCliente(curso) {
+   let usuario = prompt("Introduce tu nombre por favor:");
+    if(((usuario !== null) || (usuario !=="")) && (CURSOS.includes(curso))) {
+       USUARIOS_PROGRAMAS.push(new Cliente(usuario,curso));
+       alert(`Gracias ${usuario}, nos pondrémos en contacto contigo pronto para revisar los siguientes pasos del proceso`);
+    } else {alert("Por favor vuelva a intentarlo con un nombre válido")}
+}
 
 
 // CALCULADORA DE MAXIMO PESO EN UNA REPETICIÓN AL LEVANTAR PESAS (1RM - 1 REP MAX)
 
-function promptUsuario() {
+function calculadoraOneRepMax() {
     let promptEjercicio = prompt("Introduzca el ejercicio a calcular");
     let promptReps = parseFloat(prompt(`Introduzca el número de repeticiones que hace ${promptEjercicio} normalmente.`));
     let promptPeso = parseFloat(prompt(`Introduzca el peso en kg con el que hace ${promptReps} repecitiones normalmente.`));
