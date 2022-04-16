c3546a8090b5b9adb6dbc3965b2fc44b3a576837
Alejandro Diaz <alejandrodiazpugh@gmail.com>
2022-04-16 15:17:57 -0500

    Desafio Eventos DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// CAPTURA DE DATOS PARA PERSONAS INTERESADAS EN PROGRAMAS EN LINEA
-
 const USUARIOS_PROGRAMAS = [];
 const CURSOS = [];
 
@@ -11,32 +9,57 @@ nuevoCurso("Boring But Big");
 
 //Creamos la lista de clientes por objetos.
 class Cliente {
-    constructor(nombre, curso) {
+    constructor(nombre, apellido, correo, curso) {
         this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
         this.curso = curso;
-    }    
+    } 
 }  
 
-//Pasamos la clase por una función que agrega objetos al array de usuarios con el botón
+// TODO: FORMULARIO COMPLETO QUE AGREGUE DATOS AL ARRAY CON EL LOCAL STORAGE
 
-function agregarCliente(curso) {
-    let usuario = prompt("Introduce tu nombre por favor:");
-    if(usuario !== null && usuario !== "") {
-       USUARIOS_PROGRAMAS.push(new Cliente(usuario,curso));
-       alert(`Gracias ${usuario}, nos pondrémos en contacto contigo pronto para revisar los siguientes pasos del proceso`);
-    } else {
-        alert("Por favor introduce un nombre válido");
-    } 
+// CALCULADORA DE MAXIMO PESO EN UNA REPETICIÓN AL LEVANTAR PESAS (1RM - 1 REP MAX) -- ARREGLAR
+
+function calculadoraOneRepMax(calculadora) {
+    let calculator = document.querySelector(calculadora);
+    let ejercicio = document.querySelector("#notificaciones__ejercicios").value;
+    let reps = parseFloat(document.querySelector("#notificaciones__repeticiones").value);
+    let peso = parseFloat(document.querySelector("#notificaciones__peso").value);
+    calculator.addEventListener("click", () => {
+           let maxPeso = Math.round(peso / (1.0278 - 0.0278 * reps));
+           let mensaje = document.createElement("p");
+           mensaje.innerHTML = `<p> Tu peso máximo para ${ejercicio} es de ${maxPeso} kg.</p>`
+           document.body.appendChild(mensaje);
+    })
 }
 
+calculadoraOneRepMax("#ORMCalculator");
 
-// CALCULADORA DE MAXIMO PESO EN UNA REPETICIÓN AL LEVANTAR PESAS (1RM - 1 REP MAX)
 
-function calculadoraOneRepMax() {
-    let promptEjercicio = prompt("Introduzca el ejercicio a calcular");
-    let promptReps = parseFloat(prompt(`Introduzca el número de repeticiones que hace ${promptEjercicio} normalmente.`));
-    let promptPeso = parseFloat(prompt(`Introduzca el peso en kg con el que hace ${promptReps} repecitiones normalmente.`));
-    let orm = Math.round(promptPeso / (1.0278 - 0.0278 * promptReps));
-    let alertORM = alert(`Su máximo de peso para una repetición (ORM) es ${orm}kg`);
-    return orm;
+// MODALES CON MAS INFORMACIÓN
+
+function modalOpen(openId, modalId) {
+    const opener = document.querySelector(openId);
+    const modal = document.querySelector(modalId);
+    opener.addEventListener("click", () => {
+        modal.showModal();
+    })
 }
+function modalClose(closeId, modalId) {
+    const closer = document.querySelector(closeId);
+    const modal = document.querySelector(modalId);
+    closer.addEventListener("click", () => {
+        modal.close();
+    })
+}
+
+modalOpen("#correr","#correr__modal");
+modalClose("#correr__cerrar", "#correr__modal");
+
+modalOpen("#yoga","#yoga__modal");
+modalClose("#yoga__cerrar","#yoga__modal");
+
+modalOpen("#pesas","#pesas__modal");
+modalClose("#pesas__cerrar","#pesas__modal");
+
